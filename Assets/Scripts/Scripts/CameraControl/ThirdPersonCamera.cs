6a58c2522176fa5385f30deac175773265396a0b
Andre-Pires <andrepires9181@gmail.com>
2016-08-05 18:22:33 +0100

    Added the capability to switch between a bird's eye view mode and a follow object mode for the camera. Added a few icons to the GUI, making it more perceivable.

--- Assets/Scripts/Scripts/CameraControl/ThirdPersonCamera.cs
+++ Assets/Scripts/Scripts/CameraControl/ThirdPersonCamera.cs
@@ -7,9 +7,14 @@ namespace Assets.Scripts.Scripts.CameraControl
 
         private const float MIN_Y_ANGLE = 10.0f;
         private const float MAX_Y_ANGLE = 70.0f;
+        private const float MIN_ZOOM = 5.0f;
+        private const float MAX_ZOOM = 75.0f;
 
-        public Transform LookAtTarget;
+        public Transform BirdViewLookAt;
+        public Transform CloseUpLookAt;
         private Camera _camera;
+        private bool _birdEyeViewActive = true;
+        private const float BIRD_EYE_VIEW_DIST = 50.0f;
 
         private float currentDistance = 50.0f;
         private float zoomSensitivity = 50.0f;
@@ -26,14 +31,17 @@ namespace Assets.Scripts.Scripts.CameraControl
             _camera = Camera.main;
 
             //start looking at the center of the set
-            LookAtTarget = GameObject.FindGameObjectWithTag("Scenario").transform;
+            BirdViewLookAt = GameObject.FindGameObjectWithTag("Scenario").transform;
+
+            //register listener from camera mode switch
+            UI.ChangeCameraMode.Instance.OnSelect += ChangeCameraMode;
         }
 
         private void Update()
         {
-            if (LookAtTarget == null)
+            if (BirdViewLookAt == null)
             {
-                LookAtTarget = GameObject.FindGameObjectWithTag("Scenario").transform;
+                BirdViewLookAt = GameObject.FindGameObjectWithTag("Scenario").transform;
             }
 
             //negate X in order to move in same direction as mouse
@@ -42,33 +50,57 @@ namespace Assets.Scripts.Scripts.CameraControl
                 currentX -= Input.GetAxis("Mouse X");
                 currentY += Input.GetAxis("Mouse Y");
                 currentY = Mathf.Clamp(currentY, MIN_Y_ANGLE, MAX_Y_ANGLE);
-
-                currentDistance += Input.GetAxis("Mouse ScrollWheel")*zoomSensitivity;
             }
 
-            if (Input.GetButtonUp("Fire1"))
+            //only when allowing changing lookat and zooming
+            if (!_birdEyeViewActive)
             {
-                _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                currentDistance += Input.GetAxis("Mouse ScrollWheel")*zoomSensitivity;
+                currentDistance = Mathf.Clamp(currentDistance, MIN_ZOOM, MAX_ZOOM);
 
-                if (Physics.Raycast(_ray, out _hit, 100))
+                if (Input.GetButtonUp("Fire1"))
                 {
-                    Debug.Log("New look at: " + _hit.transform.name);
-                    LookAtTarget = _hit.transform;
+                    _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    if (Physics.Raycast(_ray, out _hit, 100))
+                    {
+                        Debug.Log("New look at: " + _hit.transform.name);
+                        CloseUpLookAt = _hit.transform;
+                    }
                 }
             }
         }
 
         private void LateUpdate()
         {
-            // to account for the frame where the set changes
-            if (LookAtTarget != null)
+            Transform lookAtInUse;
+
+            if (_birdEyeViewActive)
             {
-                Vector3 dir = new Vector3(0,0,-currentDistance);
-                Quaternion rotation = Quaternion.Euler(currentY,currentX,0);
-                _camera.transform.position = LookAtTarget.position + rotation*dir;
-                _camera.transform.LookAt(LookAtTarget.position);
+                lookAtInUse = BirdViewLookAt;
             }
-            
+            else
+            {
+                lookAtInUse = CloseUpLookAt ?? BirdViewLookAt;
+            }
+
+            if (lookAtInUse != null)
+            {
+                Vector3 dir = new Vector3(0, 0, -currentDistance);
+                Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+                _camera.transform.position = lookAtInUse.position + rotation * dir;
+                _camera.transform.LookAt(lookAtInUse.position);
+            }
+        }
+
+        public void ChangeCameraMode()
+        {
+            _birdEyeViewActive = !_birdEyeViewActive;
+
+            if (_birdEyeViewActive)
+            {
+                currentDistance = BIRD_EYE_VIEW_DIST;
+            }
         }
 
     }
---
+++ Assets/Scripts/Scripts/UI/ChangeCameraMode.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Scripts.UI;
+
+namespace Assets.Scripts.Scripts.UI
+{
+    public class ChangeCameraMode : MonoBehaviour
+    {
+        // Singleton 	
+        private static ChangeCameraMode _instance;
+
+        public delegate void OnSelectEvent();
+        public event OnSelectEvent OnSelect;
+
+        // Construct 	
+        private ChangeCameraMode()
+        {
+        }
+
+        //  Instance 	
+        public static ChangeCameraMode Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = GameObject.FindObjectOfType(typeof(ChangeCameraMode)) as ChangeCameraMode;
+                return _instance;
+            }
+
+        }
+
+        // Handle our Ray and Hit
+        void Update()
+        {
+        }
+
+        public void OnClick()
+        {
+            // Notify of the event!
+            OnSelect();
+        }
+
+    }
+}
